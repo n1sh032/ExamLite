2af92fb9c2de1fc40e30ade3ed2e202c60d864ed
Nish <nishanthgobinath39@gmail.com>
2026-07-20 20:24:31 +0800

    Add backend subject storage

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+import json
+import os
 
 app = Flask(__name__)
 
@@ -6,5 +8,25 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/add_subject", methods=["POST"])
+def add_subject():
+
+    data = request.json
+
+    file = "data/subjects.json"
+
+    if os.path.exists(file):
+        with open(file, "r") as f:
+            subjects = json.load(f)
+    else:
+        subjects = []
+
+    subjects.append(data)
+
+    with open(file, "w") as f:
+        json.dump(subjects, f, indent=4)
+
+    return jsonify({"message":"saved"})
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True) 
--- static/app.js
+++ static/app.js
@@ -1,1 +1,36 @@
-console.log("js connected");
+let btn = document.getElementById("addBtn");
+
+btn.addEventListener("click", function(){
+
+    let subject = document.getElementById("subject").value;
+    let cu = document.getElementById("cu").value;
+    let priority = document.getElementById("priority").value;
+    let exam = document.getElementById("exam").value;
+
+    if(subject == "" || cu == "" || exam == ""){
+        alert("Fill in all fields");
+        return;
+    }
+
+    fetch("/add_subject", {
+        method:"POST",
+        headers:{
+            "Content-Type":"application/json"
+        },
+        body: JSON.stringify({
+            subject: subject,
+            cu: cu,
+            priority: priority,
+            exam: exam
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        alert("Subject Saved");
+
+        document.getElementById("subject").value = "";
+        document.getElementById("cu").value = "";
+        document.getElementById("exam").value = "";
+    });
+
+});
